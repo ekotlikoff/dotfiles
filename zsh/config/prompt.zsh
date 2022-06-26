81a48855e0bfc68225460d67b891cf0acedf6c72
ekotlikoff <ekotlikoff@gmail.com>
2022-06-26 01:39:10 +0100

    Add zsh prompt plugin

--- zsh/config/prompt.zsh
+++ zsh/config/prompt.zsh
@@ -1,12 +1,3 @@
-autoload colors && colors
-
-# Add vcs info to promp as per https://git-scm.com/book/en/v2/Appendix-A%3A-Git-in-Other-Environments-Git-in-Zsh
-autoload -Uz vcs_info
-precmd_vcs_info() { vcs_info }
-precmd_functions+=( precmd_vcs_info )
-setopt prompt_subst
-RPROMPT=\$vcs_info_msg_0_
-# PROMPT=\$vcs_info_msg_0_'%# '
-zstyle ':vcs_info:git:*' formats '%b'
-# Add working directory to prompt
-export PS1="%d %% "
+fpath+=$DOT/zsh/plugin/typewritten
+autoload -U promptinit; promptinit
+prompt typewritten
