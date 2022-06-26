ffc8fcd5449bb65b9a1af2d6d98223c673429bdc
ekotlikoff <ekotlikoff@gmail.com>
2022-06-26 10:35:24 +0100

    Add vim-commentary

--- vim/install.sh
+++ vim/install.sh
@@ -7,19 +7,15 @@ set -e
 # Check for Vim
 if test $(which vim)
 then
-  echo '  Installing CoC.'
+  echo '  Installing node for CoC.'
   brew install node
-  vim -esN +'helptags $HOME/.vim/pack/plugin/start/coc.nvim/doc/' +qall
-  vim -esN +'helptags $HOME/.vim/pack/plugin/start/nerdtree/doc/' +qall
   echo '  Installing CoC extensions.'
   coc_extensions='coc-json coc-html coc-go coc-jedi' # coc-java
   vim -esN +"CocInstall -sync $coc_extensions | q" +qall
-
-  # Run GoInstallBinaries vim command (provided by
-  # https://github.com/fatih/vim-go/blob/master/doc/vim-go.txt) to install Go
-  # tool binaries under g:go_bin_path.
-  # echo '  Running GoInstallBinaries'
-  # vim -esN +GoInstallBinaries +qall
+  echo '  Installing helptags'
+  vim -esN +'helptags $HOME/.vim/pack/plugin/start/vim-commentary/doc/' +qall
+  vim -esN +'helptags $HOME/.vim/pack/plugin/start/coc.nvim/doc/' +qall
+  vim -esN +'helptags $HOME/.vim/pack/plugin/start/nerdtree/doc/' +qall
 fi
 
 exit 0
