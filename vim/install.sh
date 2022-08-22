3eea80c634ab1a12ba54105fabb592bcf6123076
ekotlikoff <ekotlikoff@gmail.com>
2022-08-22 15:32:15 +0100

    Add coc-java

--- vim/install.sh
+++ vim/install.sh
@@ -10,7 +10,7 @@ then
   echo '  installing node for CoC'
   brew install node
   echo '  installing CoC extensions'
-  coc_extensions='coc-json coc-html coc-go coc-jedi' # coc-java
+  coc_extensions='coc-json coc-html coc-go coc-jedi coc-java'
   vim -esN +"CocInstall -sync $coc_extensions | q" +qall
   echo '  installing helptags'
   for plugin in $(find -H "$HOME/.vim/pack/plugin/start/" -maxdepth 2 -name 'doc'); do
