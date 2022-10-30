a037bddda67c01ae4ff087b1820b7b46d7f20144
ekotlikoff <ekotlikoff@gmail.com>
2022-10-29 22:32:11 -0400

    Turn off typewritten for corp

--- zsh/config/typewritten.zsh
+++ zsh/config/typewritten.zsh
@@ -1,4 +1,6 @@
-fpath+=$DOT/zsh/plugin/typewritten
-autoload -U promptinit; promptinit
-prompt typewritten
-TYPEWRITTEN_CURSOR="block"
+if [ "$DOT_TYPE" != 'corp' ]; then
+  fpath+=$DOT/zsh/plugin/typewritten
+  autoload -U promptinit; promptinit
+  prompt typewritten
+  TYPEWRITTEN_CURSOR="block"
+fi
